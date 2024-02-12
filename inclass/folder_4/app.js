46091fe65f93ff8eb17b30c27745e8d996c710f8
tarik <63872091+tariksitki@users.noreply.github.com>
2024-02-12 23:49:00 +0100

    12.02.2024

---
+++ inclass/folder_4/app.js
@@ -0,0 +1,26 @@
+// var hasan = document.getElementById("div_2");
+// var sedat = document.getElementById("div_1");
+// var button = document.getElementById("button");
+
+// button.onclick = function() {
+//     hasan.style.backgroundColor = "yellow";
+//     if (hasan.style.backgroundColor == "yellow") {
+//         sedat.style.height = "300px";
+//     }
+// }
+// hasan.onclick = function() {
+//     sedat.style.backgroundColor = "red";
+// }
+
+// sedat.onclick = function() {
+//     hasan.style.width = "40%";
+// }
+
+// hasan.onmouseenter = function() {
+//     hasan.style.backgroundColor = "yellow";
+// }
+
+// hasan.onmouseout = function() {
+//     hasan.style.backgroundColor = "brown";
+// }
+
