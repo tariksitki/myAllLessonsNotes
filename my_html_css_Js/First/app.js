4401e3da6a4ec3de8fae41ba16ca8f7c46634f05
tarik <63872091+tariksitki@users.noreply.github.com>
2024-10-04 18:23:13 +0200

    04.10.2024

--- my_html_css_Js/First/app.js
+++ my_html_css_Js/First/app.js
@@ -4,6 +4,6 @@ var p = document.getElementById("p");
 p.onclick = function() {
     p.style.backgroundColor = "red";
     p.style.color = "yellow";
-    p.style.fontSize = "larger";
+    p.style.fontSize = "larger";      
 }
 
